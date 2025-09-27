834fd85bb063d9c7275f99bc2199ede34563a612
tiekiran2008 <kiran08461kumar@gmail.com>
2025-09-28 00:38:59 +0530

    version 1

---
+++ def add.py
@@ -0,0 +1,21 @@
+def add():
+                print("Addition of two numbers")
+                a=(int(input("Enter a: ")))
+                b=(int(input("Enter b: ")))
+                print(a+b)
+add()
+def sub():
+                print("Subtraction of two numbers")
+                a=(int(input("Enter a: ")))
+                b=(int(input("Enter b: ")))
+                print(a-b)
+def mul():
+                print("Multiplication of two numbers")
+                a=(int(input("Enter a: ")))
+                b=(int(input("Enter b: ")))
+                print(a*b)
+def div():
+                print("Division of two numbers")
+                a=(int(input("Enter a: ")))
+                b=(int(input("Enter b: ")))
+                print(a/b)
